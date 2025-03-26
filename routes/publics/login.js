8b27f268eab94a1d5bf32039facd683b44aaeaa9
ViniciusDev <viniholanda49@gmail.com>
2025-03-26 10:07:37 -0300

    created router login user

--- routes/publics/login.js
+++ routes/publics/login.js
@@ -5,11 +5,26 @@ const router = express.Router()
 const prisma = new PrismaClient();
 
 
-router.post('/login', async () => {
+router.post('/login', async (req, res) => {
 
     try{
 
+        const {email , password} = req.body
 
+        const user = await prisma.user.findUnique({
+            where: {
+                email : email,
+            }
+        })
+
+        if(!user){
+            return res.status(401).json({ error: 'Usuário não encontrado'})
+        }
+        if(user.password !== password){
+            return res.status(401).json({ error: 'Senha inválida'})
+        }
+
+        res.status(200).json({ message: "usuario logado com sucesso" })
 
     } catch (error) {
         console.log(error)
